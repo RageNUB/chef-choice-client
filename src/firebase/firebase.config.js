51d0a1920c327adc741f81d11952398dbb7455b1
RageNUB <jirimon39@gmail.com>
2023-05-02 18:43:19 +0600

    navbar added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCqBrnozCHzO-5U_GuGoGDHU5NxWkRNJ8A",
+  authDomain: "chef-choice-ab13b.firebaseapp.com",
+  projectId: "chef-choice-ab13b",
+  storageBucket: "chef-choice-ab13b.appspot.com",
+  messagingSenderId: "458305522023",
+  appId: "1:458305522023:web:3e4de2fbc4029eb74247c2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
